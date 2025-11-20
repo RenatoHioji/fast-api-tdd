41093d09ace76b32dba8c10d3b9b5f45f8edfa30
RenatoHioji <okamoto.hioji@gmail.com>
2025-11-20 13:20:34 -0300

    feat: adiiconando rota de busca de items

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+fake_db = {
+    1: {"id": 1, "name": "Item 1"},
+    2: {"id": 2, "name": "Item 2"},
+}
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int):
+    item = fake_db.get(item_id)
+    if not item:
+        raise HTTPException(status_code=404, detail="Item não existe")
+
+    return item
